efaa1cd87d347fbd396641e764cd2bc16b7eb3e1
Sam Beckham <sam@sambeckhamdesign.com>
2014-07-21 23:15:24 +0100

    Added the basics for fetching images from Filkr

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/:width/:height?', function(req, res) {
-	console.log(req.params);
-	var width = req.params.width,
-		height = req.params.height || width;
+router.get('/:tags/:width/:height?', function(req, res) {
+	var tags = req.params.tags,
+		width = req.params.width,
+		height = req.params.height || width,
+		photo,
+		url;
 
-  res.render('index', { image: 'http://placehold.it/' + width + 'x' + height });
+// s	small square 75x75
+// q	large square 150x150
+// t	thumbnail, 100 on longest side
+// m	small, 240 on longest side
+// n	small, 320 on longest side
+// -	medium, 500 on longest side
+// z	medium 640, 640 on longest side
+// c	medium 800, 800 on longest side†
+// b	large, 1024 on longest side*
+
+	var Flickr = require("node-flickr"),
+		keys = {"api_key": "76616ca9d5565c4e97fd67e65d4b4ebd"},
+		flickr = new Flickr(keys);
+
+	flickr.get("photos.search", {"tags": tags}, function(result){
+	    photo = result.photos.photo[0];
+	    url = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_b.jpg';
+	    res.render('index', { image: url });
+	});
 });
 
 module.exports = router;
