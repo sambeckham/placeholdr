86b8d3cc417b5e18f04291686371ef53d76c46ce
Sam Beckham <sambeckham@users.noreply.github.com>
2014-07-22 16:15:27 +0100

    Revert "Develop"

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var flickr = require('../controllers/flickr');
 
 /* GET home page. */
-router.get('/:tags/:width/:height?', function(req, res) {
-	var tags = req.params.tags,
-		width = parseInt(req.params.width),
-		height = parseInt(req.params.height) || width,
-		size = flickr.getSize(width, height),
-		url = flickr.getPhoto(tags, size, function() {
-			res.render('index', { image: this });
-		});
+router.get('/:width/:height?', function(req, res) {
+	console.log(req.params);
+	var width = req.params.width,
+		height = req.params.height || width;
+
+  res.render('index', { image: 'http://placehold.it/' + width + 'x' + height });
 });
 
 module.exports = router;
