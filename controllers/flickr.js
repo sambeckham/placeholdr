de38d7e4c116e4234611a66bb593d6ee81a7be8b
samdbeckham <sam@drummondcentral.co.uk>
2014-07-22 16:06:41 +0100

    Added a size param to pass into the flickr API

--- controllers/flickr.js
+++ controllers/flickr.js
@@ -1,16 +1,27 @@
-// Sizes (by flickr)
-// ==========
-//
-// url_sq : 75 square.
-// url_q : 150 square.
-//
-// url_t : 100 on longest side.
-// url_s : 240 on longest side.
-// url_n : 320 on longest side.
-// url_m : 500 on longest side.
-// url_z : 640 on longest side.
-// url_c : 800 on longest side.
-// url_l : 1024 on longest side.
+this.getSize = function(width, height){
+    var largestSize = (width > height) ? width : height,
+        sizes = {
+            100 : 'url_t',
+            240 : 'url_s',
+            320 : 'url_n',
+            500 : 'url_m',
+            640 : 'url_z',
+            800 : 'url_c',
+            1024 : 'url_l',
+        },
+        result = sizes[100];
+
+    for (var size in sizes){
+        if(largestSize < size){
+            result = sizes[size];
+            break;
+        }
+        result = sizes[1024];
+    }
+
+    return result;
+};
+
 
 this.getPhoto = function(tags, size, callback){
     var FlickrApi = require("node-flickr"),
@@ -22,9 +33,9 @@ this.getPhoto = function(tags, size, callback){
         "content_type": 1, // Only get photographs
         "safe_search": 1, // No naughty images
         "privacy_filter": 1, // Only public images
-        "tag_mode": 'all' // Search tags with AND argument
-        "extras": size, // Pull in the size param
-        "tags": tags, // Pull in the tag param
+        "tag_mode": 'all', // Search tags with AND argument
+        "extras": size + ', o_dims', // Pull in the size param and the original dimensions
+        "tags": tags, // Pull in the tags param
     };
 
     flickrApi.get("photos.search", properties, function(result){
@@ -33,6 +44,6 @@ this.getPhoto = function(tags, size, callback){
 
         photo = result.photos.photo[i];
         url = photo[size];
-        callback.call(url); // Send the photo URL to the callback function
+        callback.call(url);
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,10 @@ var flickr = require('../controllers/flickr');
 /* GET home page. */
 router.get('/:tags/:width/:height?', function(req, res) {
 	var tags = req.params.tags,
-		width = req.params.width,
-		height = req.params.height || width,
-		url = flickr.getPhoto(tags, 'url_l', function() {
+		width = parseInt(req.params.width),
+		height = parseInt(req.params.height) || width,
+		size = flickr.getSize(width, height),
+		url = flickr.getPhoto(tags, size, function() {
 			res.render('index', { image: this });
 		});
 });
